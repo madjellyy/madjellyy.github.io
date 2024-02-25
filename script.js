b3935a19ab344755a1c0300b13cc23a65f2aaa20
madjellyy <161178391+madjellyy@users.noreply.github.com>
2024-02-25 18:07:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
         "background3.gif",
         "background4.gif",
         "background5.gif"
+        "background6.gif"
         // Add more image URLs as needed
     ];
 
