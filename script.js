e095110275ff3828cbf66545b1a2ca70c2aa2aa3
madjellyy <161178391+madjellyy@users.noreply.github.com>
2024-02-27 12:35:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function() {
     ];
 
     const randomIndex = Math.floor(Math.random() * images.length);
-    const selectedImage = images[randomIndex];
+    const selectedImage = images[randomIndex];   
     const backgroundElement = document.querySelector('.background-image');
     backgroundElement.style.backgroundImage = `url('${selectedImage}')`;
 });
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.body.addEventListener('click', function() {
     var audio = document.getElementById('myAudio');
     if (audio) {
-      audio.volume = 2;
+      audio.volume = 1;
       audio.play();
     
   });
