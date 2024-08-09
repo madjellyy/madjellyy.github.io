9ca1cb400f645bd6637d2c8e286979bd9e36ab1c
btxpoly <btxpoly@gmail.com>
2024-08-09 13:40:57 +0100

    Delete audio.js

--- audio.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.body.addEventListener('click', function() {
-      var audio = document.getElementById('myAudio');
-      if (audio) {
-        audio.volume = 1;
-        audio.play();
-      }
-    });
-  });
