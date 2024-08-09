3ef750c39a452992c321e7559724cf04ce079a53
btxpoly <btxpoly@gmail.com>
2024-08-09 20:12:27 +0100

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,4 +1,4 @@
-document.getElementById('playImage').addEventListener('click', function() {
+document.addEventListener('click', function() {
     var audio = document.getElementById('audio');
     audio.currentTime = 0; // Reset the audio to the start
     audio.play();          // Play the audio
