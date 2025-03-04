e61262eca640e16f252cdc1a7ed4185d07d3ba30
mad jelly <161178391+madjellyy@users.noreply.github.com>
2025-03-03 22:44:55 -0500

    Update js_stands_for_juice_sack.js

--- js_stands_for_juice_sack.js
+++ js_stands_for_juice_sack.js
@@ -1,18 +1,12 @@
-console.log("sigma");
-//fyi if ur reading this, consider doing all three
-// find god
-// get a job
-// and GET OUT!
-const images = [
-    "i_best_cat.webp",
-    "i_cat_mogus.jpg",
-    "i_cup_cat.png",
-    "i_daisy_cat.webp"
-];
-
-let currentIndex = 0;
-
 document.addEventListener('DOMContentLoaded', () => {
+    const images = [
+        "i_best_cat.webp",
+        "i_cat_mogus.jpg",
+        "i_cup_cat.png",
+        "i_daisy_cat.webp"
+    ];
+
+    let currentIndex = 0;
     const imageElement = document.getElementById('current-image');
 
     if (!imageElement) {
@@ -25,12 +19,16 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Current Index:', currentIndex);
 
         currentIndex = (currentIndex + 1) % images.length;
-        imageElement.src = images[currentIndex];
+        const newImage = images[currentIndex];
 
-        console.log('New Image Path:', images[currentIndex]);
+        imageElement.src = newImage;
+
+        document.body.style.backgroundImage = `url(${newImage})`;
+
+        console.log('New Image Path:', newImage);
     };
 
     imageElement.addEventListener('pointerdown', changeImage);
-    imageElement.addEventListener('touchstart',changeImage);
-});
 
+    document.body.style.backgroundImage = `url(${images[0]})`;
+});
